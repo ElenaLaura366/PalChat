da446f940ac061b87b413c8d47da0db6189ea83c
Gârcineanu Elena-Laura <lauraelena366@gmail.com>
2025-08-06 16:38:34 +0300

    Made the UI for login and register

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,11 +2,22 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 p-4">
-      <Link to="/" className="text-white font-bold">PalChat</Link>
-      <Link to="/Login" className="text-white font-bold">Login</Link>
+    <nav className="bg-indigo-600 p-4 flex justify-between items-center">
+      <div>
+        <Link to="/" className="text-white font-bold">
+          PalChat
+        </Link>
+      </div>
+      <div className="flex space-x-4">
+        <Link to="/Login" className="text-white font-bold">
+          Login
+        </Link>
+        <Link to="/Register" className="text-white font-bold">
+          Register
+        </Link>
+      </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
--- frontend/src/routes/Login.tsx
+++ frontend/src/routes/Login.tsx
@@ -1,10 +1,26 @@
 import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <>
       <Navbar />
-      <div>Login</div>
+      <div className="flex flex-col mt-32 ms-16">
+        <h1 className="text-6xl font-semibold">Welcome back</h1>
+        <div className="mt-4 flex">
+          <h2 className="font-thin ms-2 me-1">New to PalChat?</h2>
+          <Link to="/Register" className="text-indigo-600 font-bold">
+            Register
+          </Link>
+        </div>
+        <div className="mt-8 w-1/3">
+          <form className="flex flex-col gap-4">
+            <input type="email" placeholder="Email" className="p-4 border border-gray-200 bg-gray-200 rounded-xl"></input>
+            <input type="password" placeholder="Password" className="p-4 border border-gray-200 bg-gray-200 rounded-xl"></input>
+            <button type="button" className="bg-indigo-600 hover:bg-indigo-700 text-gray-50 py-4 rounded-xl cursor-pointer">LogIn</button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
